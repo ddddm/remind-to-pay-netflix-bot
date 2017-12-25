41c7d7fc4d6119f1118869d74746c134e4be491d
Dmitry Melnikov <m1290d@gmail.com>
2017-12-25 14:43:51 +0500

    Simple Telegram event handler

--- handler.ts
+++ handler.ts
@@ -1,11 +1,38 @@
-export const hello = (event, context, cb) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+import { getBinBankExchangeRates, getPaymentShares } from './exchangeRate';
+import { sendMessage } from './telegram';
+const config =  require("./config.json");
 
-  cb(null, response);
+export const telegramWebhookHandler = async (event, context, cb) => {
+  let payload;
+
+  cb(null, {
+    statusCode: 200,
+  });
+
+  try {
+    payload = JSON.parse(event.body);
+    const { message = {} } = payload || {};
+    const { chat = {} } = message || {};
+  
+    if(chat.id) {
+      const rate = await getBinBankExchangeRates();
+      sendMessage(
+        chat.id,
+        [
+          'Если бы за Netflix платить пришлось сегодня, то по',
+          getPaymentShares(rate * config.payment_in_euro),
+          '₽',
+          'с человека.'
+        ].join(' ')
+      );
+    }
+  } catch (error) {
+    return context.fail()
+  }
+  
+
+}
+
+export const reminder = (event, context, cb) => {
+  // this is called when need to remind about payment
 }
