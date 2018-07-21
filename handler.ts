ad6b3ef7cc0e8c6b754e74e98fdbf743407920e5
Dmitry Melnikov <m1290d@gmail.com>
2018-07-21 18:54:13 +0500

    Use telegraf for telegram apis

--- handler.ts
+++ handler.ts
@@ -1,36 +1,59 @@
 import { getBinBankExchangeRates, getPaymentShares } from './exchangeRate';
-import { sendMessage } from './telegram';
+import * as firebase from 'firebase-admin';
+import * as Telegraf from 'telegraf';
+
 const config =  require("./config.json");
+const certFile = require(`./keys/${config.firebase.key_file}`);
+
+firebase.initializeApp({
+  databaseURL: config.firebase.database,
+  credential: firebase.credential.cert(certFile) 
+})
 
 export const telegramWebhookHandler = async (event, context, cb) => {
+  const app = new Telegraf(config.token);
   let payload;
 
-  cb(null, {
-    statusCode: 200,
+  app.command('payment', async (ctx) => {
+    const rate = await getBinBankExchangeRates();
+    ctx.reply(
+      [
+        'Если бы за Netflix платить пришлось сегодня, то по',
+        getPaymentShares(rate * config.payment_in_euro),
+        "рублей",
+        'с человека.'
+      ].join(' ')
+    );
+    cb(null, {
+      statusCode: 200,
+    });
   });
+  app.command('start', async (ctx) => {
+    const chatId = ctx.chat.id;
+    const db = firebase.database();
+    const chatsRef = db.ref("chats");
+    
+    ctx.reply('Я запомнил этот чат и буду слать в него напоминания.');
+    chatsRef.child(chatId.toString()).set(new Date().toString())
+
+    cb(null, {
+      statusCode: 200,
+    });
+  })
+  app.on('text', (ctx) => {
+    ctx.reply('Hello World');
+    cb(null, {
+      statusCode: 200,
+    });
+  })
 
   try {
     payload = JSON.parse(event.body);
-    const { message = {} } = payload || {};
-    const { chat = {} } = message || {};
-  
-    if(chat.id) {
-      const rate = await getBinBankExchangeRates();
-      sendMessage(
-        chat.id,
-        [
-          'Если бы за Netflix платить пришлось сегодня, то по',
-          getPaymentShares(rate * config.payment_in_euro),
-          "рублей",
-          'с человека.'
-        ].join(' ')
-      );
-    }
+    app.handleUpdate(payload);
   } catch (error) {
+    console.error(error);
     return context.fail()
   }
-  
-
 }
 
 export const reminder = (event, context, cb) => {
