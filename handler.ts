ff329043acdef3d9d27992181c580928e59655e8
Dmitry Melnikov <m1290d@gmail.com>
2018-09-12 23:04:08 +0500

    Remove not working 'start' handler

--- handler.ts
+++ handler.ts
@@ -1,14 +1,7 @@
 import { getBinBankExchangeRates, getPaymentShares } from './exchangeRate';
-import * as firebase from 'firebase-admin';
 import * as Telegraf from 'telegraf';
 
 const config =  require("./config.json");
-const certFile = require(`./keys/${config.firebase.key_file}`);
-
-firebase.initializeApp({
-  databaseURL: config.firebase.database,
-  credential: firebase.credential.cert(certFile) 
-})
 
 export const telegramWebhookHandler = async (event, context, cb) => {
   const app = new Telegraf(config.token);
@@ -28,18 +21,6 @@ export const telegramWebhookHandler = async (event, context, cb) => {
       statusCode: 200,
     });
   });
-  app.command('start', async (ctx) => {
-    const chatId = ctx.chat.id;
-    const db = firebase.database();
-    const chatsRef = db.ref("chats");
-    
-    ctx.reply('Я запомнил этот чат и буду слать в него напоминания.');
-    chatsRef.child(chatId.toString()).set(new Date().toString())
-
-    cb(null, {
-      statusCode: 200,
-    });
-  })
   app.on('text', (ctx) => {
     ctx.reply('Hello World');
     cb(null, {
