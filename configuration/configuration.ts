1ec2ac8575a60bc35bca08787b517c4e45a36733
Dmitry Melnikov <m1290d@gmail.com>
2020-05-17 06:05:15 +0500

    aaand fix the typescript error

--- configuration/configuration.ts
+++ configuration/configuration.ts
@@ -1,16 +1,23 @@
-import * as assert from 'assert';
+let token = process.env.TELEGRAM_BOT_TOKEN;
+let numberOfPeople = parseInt(process.env.DEFAULT_NUMBER_OF_PEOPLE || '3');
+let payment = parseFloat(process.env.PAYMENT_IN_EURO || '11.99');
+let dbName = process.env.DOCUMENT_STORAGE_DB_NAME;
+let collectionName = process.env.DOCUMENT_STORAGE_COLLECTION_NAME;
+let connectionString = process.env.DOCUMENT_STORAGE_CONNECTION_STRING;
 
-const token = process.env.TELEGRAM_BOT_TOKEN;
-const numberOfPeople = parseInt(process.env.DEFAULT_NUMBER_OF_PEOPLE || '3');
-const payment = parseFloat(process.env.PAYMENT_IN_EURO || '11.99');
-const dbName = process.env.DOCUMENT_STORAGE_DB_NAME;
-const collectionName = process.env.DOCUMENT_STORAGE_COLLECTION_NAME;
-const connectionString = process.env.DOCUMENT_STORAGE_CONNECTION_STRING;
+const assert = (input: string | undefined, message: string) => {
+    if(!input) {
+        throw new Error(message)
+    }
 
-assert(dbName, 'Mongo DB name should be specified');
-assert(collectionName, 'Mongo collection name should be specified');
-assert(connectionString, 'Mongo connection string should be specified');
-assert(token, 'Telegram token string should be specified');
+    return input;
+}
+
+dbName = assert(dbName, 'Mongo DB name should be specified');
+collectionName = assert(collectionName, 'Mongo collection name should be specified');
+connectionString = assert(connectionString, 'Mongo connection string should be specified');
+token = assert(token, 'Telegram token string should be specified');
+
 
 const config = {
     dbName,
