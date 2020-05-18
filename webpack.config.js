00972d2d0ddccfea2b3604c3224d35d5d39dbbfb
Dmitry Melnikov <m1290d@gmail.com>
2020-05-18 23:39:05 +0500

    update bundle process

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,13 @@
 const path = require('path');
 const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals')
+
 
 module.exports = {
   context: __dirname,
-  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   entry: slsw.lib.entries,
-  devtool: slsw.lib.webpack.isLocal ? 'cheap-module-eval-source-map' : 'source-map',
-  resolve: {
-    extensions: ['.json', '.ts'],
-    symlinks: false,
-    cacheWithContext: false,
-  },
-  output: {
-    libraryTarget: 'commonjs',
-    path: path.join(__dirname, '.webpack'),
-    filename: '[name].js',
-  },
   target: 'node',
+  externals: [nodeExternals()],
   module: {
     rules: [
       {
@@ -28,11 +19,7 @@ module.exports = {
             path.resolve(__dirname, '.serverless'),
             path.resolve(__dirname, '.webpack'),
           ],
-        ],
-        options: {
-          transpileOnly: true,
-          experimentalWatchApi: true,
-        },
+        ]
       },
     ],
   }
